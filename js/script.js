17e9ae6aedf2e4b963b4b0a8a31cc2ea7945bb72
Renato <renlopez08@icloud.com>
2025-06-02 11:16:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,6 +11,15 @@ function upDate(previewPic){
     let imageDiv = document.getElementById("image");
         imageDiv.style.backgroundImage = "url('')";
         imageDiv.innerHTML = "Hover over an image below to display here";
- 
-		
+        imageDiv.style.backgroundColor = '#000000a0'; // fondo oscuro semitransparente para el texto
+        imageDiv.style.color = 'white';
+        imageDiv.style.padding = '20px';
 	}
+
+    window.onload = function() {
+        console.log("Page loaded successfully.");
+        let previews = document.querySelectorAll('.preview');
+        for (let i=0; i < previews.length; i++) {
+            previews [i].setAttribute('tabindex', '0')
+        }
+    }
